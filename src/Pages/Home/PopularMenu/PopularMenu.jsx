3a142082250748514824968af27bf4b2ce889e98
rehanulhb <rehanulh@gmail.com>
2025-02-13 23:45:41 +0100

    Working on Popular Menus

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+
+const PopularMenu = () => {
+    return (
+        <section>
+            <SectionTitle heading="From Our Menu" subHeading="Popular Items">
+                
+            </SectionTitle>
+        </section>
+    );
+};
+
+export default PopularMenu;
