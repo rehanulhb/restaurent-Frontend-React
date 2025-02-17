a73bd341ae5601633ca72aa4b82041c62c38864d
rehanulhb <rehanulh@gmail.com>
2025-02-17 23:48:05 +0100

    Adding Testimonials

---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+
+const Testimonials = () => {
+    return (
+       <section>
+            <SectionTitle subHeading="What our Client Say" heading="TESTIMONIALS"></SectionTitle>
+       </section>
+    );
+};
+
+export default Testimonials;
