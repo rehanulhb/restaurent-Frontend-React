e1cd75c8d2db20cc9c45295476ca5b9b2e450846
rehanulhb <rehanulh@gmail.com>
2025-02-21 01:20:30 +0100

    Show Page Name/ Title Using Helmet(React awesome component)

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,8 +1,11 @@
-
+import { Helmet } from "react-helmet-async";
 
 const Menu = () => {
     return (
         <div>
+            <Helmet>
+                <title>Restaurent | Menu</title>
+            </Helmet>
             <h2 className="text-4xl">Menu</h2>
         </div>
     );
