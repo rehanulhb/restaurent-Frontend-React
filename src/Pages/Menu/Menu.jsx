5b83cfc82be25be38351118275d18337766c1fb8
rehanulhb <rehanulh@gmail.com>
2025-02-23 02:29:04 +0100

    Created Hook for load data Based on Filter Named 'Popular'

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../Shared/Cover/Cover";
 import menuImage from "../../../src/assets/menu/banner3.jpg"
-import PopularMenu from "../Home/PopularMenu/PopularMenu";
+
 
 const Menu = () => {
     return (
@@ -10,11 +10,8 @@ const Menu = () => {
                 <title>Restaurent | Menu</title>
             </Helmet>
             <Cover img={menuImage} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImage} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImage} title="Our Menu"></Cover>
-            <PopularMenu></PopularMenu>
+            
+            
         </div>
     );
 };
