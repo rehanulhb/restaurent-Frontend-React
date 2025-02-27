93c7bb2b440f65af64a8f4158ece80b0f90b40c6
rehanulhb <rehanulh@gmail.com>
2025-02-27 21:37:44 +0100

    Working on Tab Index for Tab

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,26 +1,31 @@
 import { useState } from "react";
 import orderCoverImg from "../../../assets/shop/banner2.jpg";
 import Cover from "../../Shared/Cover/Cover";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 import useMenu from "../../../hooks/useMenu";
 import OrderTab from "../OrderTab/OrderTab";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Order = () => {
-    const [tabIndex, setTabIndex] = useState(0);
-    const [menu] = useMenu();
-    const {category} = useParams();
-    console.log(category);
-    const dessert = menu.filter(item => item.category === 'dessert');
-    const soup = menu.filter(item => item.category === 'soup');
-    const salad = menu.filter(item => item.category === 'salad');
-    const pizza = menu.filter(item => item.category === 'pizza');
-    const drinks = menu.filter(item => item.category === 'drinks');
+  const categories = ["salad", "pizza", "soup", "dessert", "drinks"];
+  const { category } = useParams();
+  const initialIndex = categories.indexOf(category);
+  const [tabIndex, setTabIndex] = useState(initialIndex);
+  const [menu] = useMenu();
 
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const drinks = menu.filter((item) => item.category === "drinks");
 
   return (
     <div>
+      <Helmet>
+        <title>Restaurent | order Food</title>
+      </Helmet>
       <Cover img={orderCoverImg} title={"Order Food"}></Cover>
       <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
         <TabList>
@@ -31,19 +36,19 @@ const Order = () => {
           <Tab>Drinks</Tab>
         </TabList>
         <TabPanel>
-            <OrderTab items={salad}></OrderTab>
+          <OrderTab items={salad}></OrderTab>
         </TabPanel>
         <TabPanel>
-            <OrderTab items={pizza}></OrderTab>
+          <OrderTab items={pizza}></OrderTab>
         </TabPanel>
         <TabPanel>
-        <OrderTab items={soup}></OrderTab>
+          <OrderTab items={soup}></OrderTab>
         </TabPanel>
         <TabPanel>
-        <OrderTab items={dessert}></OrderTab>
+          <OrderTab items={dessert}></OrderTab>
         </TabPanel>
         <TabPanel>
-        <OrderTab items={drinks}></OrderTab>
+          <OrderTab items={drinks}></OrderTab>
         </TabPanel>
       </Tabs>
     </div>
