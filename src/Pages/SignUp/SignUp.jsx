67dd2e406e18ba04af7f33f6a68690b933c70c48
rehanulhb <rehanulh@gmail.com>
2025-04-06 21:22:40 +0200

    Working on Validation using React-hook-form plugin

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,4 +1,14 @@
+import { useForm } from "react-hook-form";
+
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,37 +21,54 @@ const SignUp = () => {
           </p>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
             <fieldset className="fieldset">
               <label className="fieldset-label">Name</label>
               <input
                 type="text"
+                {...register("name", { required: true })}
                 name="name"
                 className="input"
                 placeholder="Name"
               />
+              {errors.name && (
+                <span className="text-red-600">Name is required</span>
+              )}
+
               <br />
               <label className="fieldset-label">Email</label>
               <input
                 type="email"
                 name="email"
+                {...register("email", { required: true })}
                 className="input"
                 placeholder="Email"
               />
+              {errors.email && (
+                <span className="text-red-600">Email is required</span>
+              )}
               <br />
               <label className="fieldset-label">Password</label>
               <input
                 type="password"
                 name="password"
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  maxLength: 20,
+                })}
                 className="input"
                 placeholder="Password"
               />
+              {errors.password?.type === "required" && (
+                <p className="text-red-600">Password name is required</p>
+              )}
               <div>
                 <a className="link link-hover">Forgot password?</a>
               </div>
               <button className="btn btn-neutral mt-4">Login</button>
             </fieldset>
-          </div>
+          </form>
         </div>
       </div>
     </div>
