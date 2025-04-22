8c6fad0ff29d840c242edeef1168b468234b099f
rehanulhb <rehanulh@gmail.com>
2025-04-23 01:23:35 +0200

    Used tan stack Query(Refetch) for real time update the Cart item lengths in Navbar

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -2,6 +2,7 @@ import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
+import useCart from "../../hooks/useCart";
 
 const FoodCard = ({ item }) => {
   const { name, image, price, recipe, _id } = item;
@@ -9,11 +10,12 @@ const FoodCard = ({ item }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const axiosSecure = useAxiosSecure();
+  const [, refetch] = useCart();
 
-  const handleAddToCart = (food) => {
+  const handleAddToCart = () => {
     if (user && user.email) {
       //Send cart item to the database
-      console.log(user.email, food);
+
       const cartItem = {
         menuId: _id,
         email: user.email,
@@ -31,6 +33,8 @@ const FoodCard = ({ item }) => {
             showConfirmButton: false,
             timer: 1500,
           });
+          // Refetch cart to update the cart items count
+          refetch();
         }
       });
     } else {
@@ -64,7 +68,7 @@ const FoodCard = ({ item }) => {
         <p>{recipe}</p>
         <div className="card-actions justify-end">
           <button
-            onClick={() => handleAddToCart(item)}
+            onClick={handleAddToCart}
             className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4 uppercase"
           >
             Add to Cart
