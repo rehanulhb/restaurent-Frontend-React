9d2c858bcdfd1e5deb177695bc7b8ae65e4e8ddb
rehanulhb <rehanulh@gmail.com>
2025-04-27 23:30:58 +0200

    Working on Delete Cart Item, Data Showing is Done

--- src/Pages/Dashboard/Cart/Cart.jsx
+++ src/Pages/Dashboard/Cart/Cart.jsx
@@ -5,13 +5,13 @@ const Cart = () => {
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   return (
     <div>
-      <div className="flex justify-evenly">
+      <div className="flex justify-evenly mb-8">
         <h2 className="text-4xl">Items: {cart.length}</h2>
         <h2 className="text-4xl">Total Price: {totalPrice}</h2>
         <button className="btn btn-primary">Pay</button>
       </div>
-      <div className="overflow-x-auto">
-        <table className="table">
+      <div className="overflow-x-auto ">
+        <table className="table w-full">
           {/* head */}
           <thead>
             <tr>
@@ -23,13 +23,9 @@ const Cart = () => {
             </tr>
           </thead>
           <tbody>
-            {cart.map((item) => (
+            {cart.map((item, index) => (
               <tr key={item._id}>
-                <th>
-                  <label>
-                    <input type="checkbox" className="checkbox" />
-                  </label>
-                </th>
+                <th>{index}</th>
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
@@ -40,20 +36,10 @@ const Cart = () => {
                         />
                       </div>
                     </div>
-                    <div>
-                      <div className="font-bold">Hart Hagerty</div>
-                      <div className="text-sm opacity-50">United States</div>
-                    </div>
                   </div>
                 </td>
-                <td>
-                  Zemlak, Daniel and Leannon
-                  <br />
-                  <span className="badge badge-ghost badge-sm">
-                    Desktop Support Technician
-                  </span>
-                </td>
-                <td>Purple</td>
+                <td>{item.name}</td>
+                <td>${item.price}</td>
                 <th>
                   <button className="btn btn-ghost btn-xs">details</button>
                 </th>
