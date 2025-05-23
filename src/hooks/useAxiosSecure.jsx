22e33b4a119539f2f82c143875252bd3a02136d2
rehanulhb <rehanulh@gmail.com>
2025-05-23 23:36:06 +0200

    Working on Logout Unauthorized acess and Check is Admin

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
   //request interceptor to add authorization header for every secure call to the API
   axiosSecure.interceptors.request.use(
     function (config) {
@@ -25,6 +29,9 @@ const useAxiosSecure = () => {
     (error) => {
       const status = error.response.status;
       console.log("Status Error in the interceptor", status);
+      if (status === 401 || status === 403) {
+        navigate("/login");
+      }
       return Promise.reject(error);
     }
   );
