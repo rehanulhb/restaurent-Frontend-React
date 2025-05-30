a243229a218ff522d33f2ab422dd7ba126f2da63
rehanulhb <rehanulh@gmail.com>
2025-05-31 00:46:27 +0200

    Added few Fields for Form

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,7 +1,41 @@
+import { useForm } from "react-hook-form";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
 const AddItems = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <h2 className="text-4xl">Add Item Page</h2>
+      <SectionTitle
+        heading="ädd an itemn"
+        subHeading="What's new?"
+      ></SectionTitle>
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input {...register("name")} />
+          <select {...register("category")}>
+            <option value="salad">Salad</option>
+            <option value="male">male</option>
+            <option value="other">other</option>
+          </select>
+          <select
+            {...register("category")}
+            defaultValue="Pick a color"
+            className="select"
+          >
+            <option disabled={true}>Select a Category</option>
+            <option value="salad">Salad</option>
+            <option value="pizza">Pizza</option>
+            <option value="soup">Soup</option>
+            <option value="dessert">Dessert</option>
+            <option value="drinks">Drinks</option>
+          </select>
+          <input type="submit" />
+        </form>
+      </div>
     </div>
   );
 };
