ea23058af08def91584ebc9d0f0a87067c97a53d
rehanulhb <rehanulh@gmail.com>
2025-06-06 23:48:29 +0200

    Working on Save Menu Item to server

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaUtensils, FaUtensilSpoon } from "react-icons/fa";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
@@ -9,6 +10,7 @@ const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_ke
 const AddItems = () => {
   const { register, handleSubmit } = useForm();
   const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
   const onSubmit = async (data) => {
     console.log(data);
     //Image upload to imgbb and then get an url
@@ -18,7 +20,19 @@ const AddItems = () => {
         "Content-Type": "multipart/form-data",
       },
     });
-    console.log(res.data);
+    if (res.data.success) {
+      // Now send the menu item data to the server
+      const menuItem = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        receipe: data.receipe,
+        image: res.data.dsiplay_url,
+      };
+      const menuRes = await axiosSecure.post("/menu", menuItem);
+      console.log(menuRes.data);
+    }
+    console.log("With Image Url", res.data);
   };
 
   return (
