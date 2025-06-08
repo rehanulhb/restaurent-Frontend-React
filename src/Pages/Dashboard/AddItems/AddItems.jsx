fff67704e3f89f98a6ce681f9e11a6c766d30e6c
rehanulhb <rehanulh@gmail.com>
2025-06-08 23:37:53 +0200

    Added Sweet Alert

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -3,12 +3,13 @@ import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { FaUtensils, FaUtensilSpoon } from "react-icons/fa";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
   const axiosPublic = useAxiosPublic();
   const axiosSecure = useAxiosSecure();
   const onSubmit = async (data) => {
@@ -26,11 +27,22 @@ const AddItems = () => {
         name: data.name,
         category: data.category,
         price: parseFloat(data.price),
-        receipe: data.receipe,
+        recipe: data.recipe,
         image: res.data.dsiplay_url,
       };
       const menuRes = await axiosSecure.post("/menu", menuItem);
       console.log(menuRes.data);
+      if (menuRes.data.insertedId) {
+        //Show sucess Popup
+        reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${data.name} is added to the database `,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
     }
     console.log("With Image Url", res.data);
   };
