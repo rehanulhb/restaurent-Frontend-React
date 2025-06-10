98914f1da608bc467b1e2fd9134380b9f55f4e91
rehanulhb <rehanulh@gmail.com>
2025-06-10 02:18:16 +0200

    Showed all Menu Items and Page Design Done

--- src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,12 +1,70 @@
+import { FaEdit, FaTrashAlt, FaUsers } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
 
 const ManageItems = () => {
+  const [menu] = useMenu();
+
+  const handleDeleteItem = (item) => {};
+
   return (
     <div>
       <SectionTitle
         heading="Manage All Items"
         subHeading="Hurry up"
       ></SectionTitle>
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Item Name</th>
+                <th>Price</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menu.map((item, index) => (
+                <tr key={item._id}>
+                  <td> {index + 1}</td>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle h-12 w-12">
+                          <img
+                            src={item.image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td> {item.name}</td>
+                  <td className="text-center">${item.price}</td>
+                  <td>
+                    <button className="btn btn-ghost btn-sm bg-orange-500">
+                      <FaEdit className="text-white-400 "></FaEdit>
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDeleteItem(item)}
+                      className="btn btn-ghost btn-sm bg-red-500"
+                    >
+                      <FaTrashAlt className="text-white-400"></FaTrashAlt>
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            {/* foot */}
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
