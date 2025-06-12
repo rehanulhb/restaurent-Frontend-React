4443de4034a7f61ece86a38205f6e6ba00f47ed5
rehanulhb <rehanulh@gmail.com>
2025-06-12 03:06:57 +0200

    Having Error Deleting the Menu item- Need to solve that

--- src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,11 +1,29 @@
 import { FaEdit, FaTrashAlt, FaUsers } from "react-icons/fa";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../hooks/useMenu";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
-  const [menu] = useMenu();
+  const [menu, refetch] = useMenu();
+  const axiosSecure = useAxiosSecure();
 
-  const handleDeleteItem = (item) => {};
+  const handleDeleteItem = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/menu/${item._id}`);
+        console.log(res.data);
+      }
+    });
+  };
 
   return (
     <div>
