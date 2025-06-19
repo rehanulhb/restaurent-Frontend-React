bee51d25ea4e15c17449cf455231d48a064fc43a
rehanulhb <rehanulh@gmail.com>
2025-06-19 23:29:45 +0200

    Started Working on Payment Gateway Integration

---
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -0,0 +1,17 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const Payment = () => {
+  return (
+    <div>
+      <SectionTitle
+        heading="Payment"
+        subHeading="Please Pay to eat"
+      ></SectionTitle>
+      <div>
+        <h2 className="text-4xl">Teka o Pakhi tumi uira uira aso</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
