c103fce6afdc281aaa1ee7c3195a082e18570127
rehanulhb <rehanulh@gmail.com>
2025-07-06 04:25:37 +0200

    Save payment info and delete cart items in the database

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -65,6 +65,19 @@ const CheckoutForm = () => {
       if (paymentIntent.status === "succeeded") {
         console.log("Transaction id", paymentIntent.id);
         setTransactionId(paymentIntent.id);
+
+        //Now save the payment in the database
+        const payment = {
+          email: user.email,
+          price: totalPrice,
+          transactionId: paymentIntent.id,
+          date: new Date(), //utc date convert.use moment js to
+          cartIds: cart.map((item) => item._id),
+          menuItemIds: cart.map((item) => item.menuId),
+          status: "Pending",
+        };
+        const res = await axiosSecure.post("/payments", payment);
+        console.log("Payment Saved", res.data);
       }
     }
   };
